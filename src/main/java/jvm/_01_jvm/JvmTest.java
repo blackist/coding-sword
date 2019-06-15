f38e615a742dd4c1db42beb119de960c1b61b0bc
L.L <liangl.dong@gmail.com>
2019-06-15 12:31:24 +0800

    [多线程] 死锁

--- src/main/java/jvm/_01_jvm/JvmTest.java
+++ src/main/java/jvm/_01_jvm/JvmTest.java
@@ -1,4 +1,15 @@
 package jvm._01_jvm;
 
 public class JvmTest {
+
+	private int add() {
+		int a = 1;
+		int b = 2;
+		int c = a + b;
+		return c;
+	}
+
+	public static void main(String[] args) {
+		System.out.println(new JvmTest().add());
+	}
 }
