d88bce5724d82c405d6818dffb4ad4621bae13af
LiangLiang.Dong <1075512174@qq.com>
2020-02-16 19:55:35 +0800

    [Guava]: cahces

--- src/main/java/guava/caches/_01_LoadingCache.java
+++ src/main/java/guava/caches/_01_LoadingCache.java
@@ -1,8 +1,16 @@
 package guava.caches;
 
+import com.google.common.cache.Cache;
 import com.google.common.cache.CacheBuilder;
+import com.google.common.cache.CacheLoader;
 import com.google.common.cache.LoadingCache;
 
+import org.jboss.netty.util.internal.ConcurrentHashMap;
+
+import java.util.Random;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ConcurrentMap;
+import java.util.concurrent.ExecutionException;
 import java.util.concurrent.TimeUnit;
 
 /**
@@ -14,11 +22,75 @@ import java.util.concurrent.TimeUnit;
 public class _01_LoadingCache {
 
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws ExecutionException {
 
-		LoadingCache<String, String> caches = CacheBuilder.newBuilder()
+		LoadingCache<String, Long> longCahce = CacheBuilder.newBuilder()
 				.maximumSize(1000)
 				.expireAfterWrite(10, TimeUnit.MINUTES)
-				.removalListener()
+				.build(new CacheLoader<String, Long>() {
+					@Override
+					public Long load(String key) throws Exception {
+						return computeLong();
+					}
+				});
+		long start = System.currentTimeMillis();
+		System.out.println(longCahce.get("longK1"));
+		long period = System.currentTimeMillis() - start;
+		System.out.println("Running: " + period);
+
+		start = System.currentTimeMillis();
+		System.out.println(longCahce.get("longK1"));
+		period = System.currentTimeMillis() - start;
+		System.out.println("Running: " + period);
+
+		start = System.currentTimeMillis();
+		ConcurrentMap<String, Long> cm = longCahce.asMap();
+		System.out.println(cm.get("longK2"));
+		System.out.println(longCahce.get("longK2"));
+		period = System.currentTimeMillis() - start;
+		// 153ms
+		System.out.println("Running: " + period);
+
+		start = System.currentTimeMillis();
+		System.out.println(longCahce.get("longK2"));
+		period = System.currentTimeMillis() - start;
+		// 0ms
+		System.out.println("Running: " + period);
+
+		Cache<String, Long> cache = CacheBuilder.newBuilder()
+				.maximumSize(1000)
+				.build();
+
+		// callabele
+		start = System.currentTimeMillis();
+		System.out.println(cache.get("ck", _01_LoadingCache::computeLong));
+		period = System.currentTimeMillis() - start;
+		// 0ms
+		System.out.println("Running: " + period);
+
+		start = System.currentTimeMillis();
+		System.out.println(cache.get("ck", _01_LoadingCache::computeLong));
+		period = System.currentTimeMillis() - start;
+		// 0ms
+		System.out.println("Running: " + period);
+
+		cache.put("ck1", 99L);
+		ConcurrentMap<String, Long> cacheMap= cache.asMap();
+		System.out.println(cacheMap.get("ck"));
+		cache.put("ck2", 100L);
+		System.out.println(cacheMap.get("ck2"));
+		// 显式插入1
+		cacheMap.putIfAbsent("ck3", 101L);
+		// 显示插入2  get(key,  callable) 会创建缓存
+		System.out.println(cache.get("ck3", _01_LoadingCache::computeLong));
+	}
+
+	private static Long computeLong() {
+		Random r = new Random();
+		long t = 0L;
+		for (int i = 10000000; i > 0; --i) {
+			t += r.nextInt(100);
+		}
+		return t;
 	}
 }
---
+++ src/main/java/guava/caches/_02_Eviction.java
@@ -0,0 +1,12 @@
+package guava.caches;
+
+/**
+ * TODO 内存回收
+ *
+ * @author L.L Dong<liangl.dong@qq.com>
+ * @since 2020/2/16
+ */
+public class _02_Eviction {
+
+
+}
